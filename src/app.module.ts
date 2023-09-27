3d3dcca65bdb3cceea73b42f52dd55314751254f
Gabriel Souto Maracajá <gabriel.maracaja@ccc.ufcg.edu.br>
2023-09-27 10:54:17 -0300

    Feat: Insert Code into Heroku

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+//W7cKLRz6EnvoJ8ms
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://dbUserTest:W7cKLRz6EnvoJ8ms@cluster0.i1jbs6s.mongodb.net/'),
+    UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
